ea2f3c8a6b9427f53af87f066d93848733f5e551
Anthony Sexton <anthonysexton@gmail.com>
2025-03-11 07:58:56 +1000

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,8 @@ alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias compile="commit 'compile'"
 alias version="commit 'version'"
+alias c="open $1 -a \"Cursor\""
+alias vs="open $1 -a \"Visual Studio Code\""
 
 # Directories
 alias dotfiles="cd $DOTFILES"
